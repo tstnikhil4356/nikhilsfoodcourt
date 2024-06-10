bf2b55c6bc0aab4942434e3c5bbee8c40eeaa47b
Nikhil_singh <135108227+tstnikhil4356@users.noreply.github.com>
2024-06-10 20:56:11 +0530

    Update script.js change

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function initApp() {
   products.forEach((value, key) => {
     let newDiv = document.createElement("div");
     newDiv.classList.add("item");
-    newDiv.innerHTML = `<img src="images/${value.image}" alt="images/${
+    newDiv.innerHTML = `<img src="images/1.png" alt="images/${
       value.name
     }" />
     <div class="title">${value.name}</div>
